3de656ba5232cbe7a1ab6120e91540c95422adfd
Friederike Moeser <friederike.moeser@gmail.com>
2021-04-06 16:54:04 +0200

    Set up UserList

--- src/components/User.js
+++ src/components/User.js
@@ -1,3 +1,5 @@
-function User() {}
+function User({ gender, firstName, lastName, image }) {
+  return "Hello, it's me, the user";
+}
 
 export default User;
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,3 +1,32 @@
-function UserList() {}
+import { useState, useEffect } from "react";
+import User from "./User";
+
+function UserList() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const url =
+      "https://randomuser.me/api/?inc=email,gender,name,picture&results=10";
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+      });
+  });
+
+  function renderUsers() {
+    return users.map((user) => {
+      <User
+        gender={user.results.gender}
+        firstName={user.results.name.first}
+        lastName={user.results.name.last}
+        image={user.results.picture.medium}
+      />;
+    });
+  }
+
+  return <section>{renderUsers}</section>;
+}
 
 export default UserList;
