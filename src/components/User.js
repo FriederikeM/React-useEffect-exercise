86f203bf6f35dee25d4efabf1390790f12139b01
Friederike Moeser <friederike.moeser@gmail.com>
2021-04-06 21:03:09 +0200

    First try filter solution, non funtional

--- src/components/User.js
+++ src/components/User.js
@@ -1,5 +1,21 @@
+import "./User.css";
 function User({ gender, firstName, lastName, image }) {
-  return "Hello, it's me, the user";
+  let classForGender;
+  if (gender === "female") {
+    classForGender = "female";
+  } else {
+    classForGender = "not-female";
+  }
+  return (
+    <div className={`user-info ${classForGender}`}>
+      <h3>
+        {firstName}
+        {lastName}
+      </h3>
+      <img src={image} alt="user avatar" />
+      <h6>{gender}</h6>
+    </div>
+  );
 }
 
 export default User;
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,32 +1,88 @@
 import { useState, useEffect } from "react";
 import User from "./User";
+import "./UserList.css";
 
 function UserList() {
   const [users, setUsers] = useState([]);
+  const [amount, setAmount] = useState();
+  const [filter, setFilter] = useState("");
 
+  const baseUrl = "https://randomuser.me/api/";
+  let url = baseUrl;
   useEffect(() => {
-    const url =
-      "https://randomuser.me/api/?inc=email,gender,name,picture&results=10";
+    if (amount) {
+      url = `${baseUrl}${amount}`;
+    }
 
     fetch(url)
-      .then((response) => response.json())
+      .then((res) => res.json())
       .then((data) => {
-        setUsers(data);
+        setUsers(data.results);
       });
-  });
+  }, [amount]);
 
-  function renderUsers() {
-    return users.map((user) => {
-      <User
-        gender={user.results.gender}
-        firstName={user.results.name.first}
-        lastName={user.results.name.last}
-        image={user.results.picture.medium}
-      />;
-    });
+  function handleAmountOfUsersDisplayed(event) {
+    event.preventDefault();
+    let input = event.target;
+    setAmount(`?results=${input.value}`);
   }
 
-  return <section>{renderUsers}</section>;
+  useEffect(() => {
+    if (filter) {
+      url = `${baseUrl}${filter}${amount}`;
+    }
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data.results);
+      });
+  }, [filter]);
+
+  function handleAllFilter() {
+    setFilter("");
+  }
+
+  function handleFemaleFilter() {
+    setFilter("?gender=female");
+  }
+
+  function handleMaleFilter() {
+    setFilter("?gender=male");
+  }
+
+  function renderUsers() {
+    return users.map((user) => (
+      <User
+        gender={user.gender}
+        firstName={user.name.first}
+        lastName={user.name.last}
+        image={user.picture.medium}
+      />
+    ));
+  }
+
+  return (
+    <section>
+      <h1>Random Users</h1>
+      <input
+        type="text"
+        placeholder="how many results would you like to see?"
+        onChange={handleAmountOfUsersDisplayed}
+        class="input"
+      />
+      <button class="all" onClick={handleAllFilter}>
+        All
+      </button>
+      <button class="females" onClick={handleFemaleFilter}>
+        Female
+      </button>
+      <button class="males" onClick={handleMaleFilter}>
+        Male
+      </button>
+      {renderUsers()}
+    </section>
+  );
 }
 
 export default UserList;
